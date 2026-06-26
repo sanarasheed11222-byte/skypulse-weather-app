508c8297ad34e8392fc763c7ea7c3b5d5c5341b7
sanarasheed11222-byte <sanarasheed11222@gmail.com>
2026-06-26 16:41:09 +0500

    Fix backend URL

--- frontend/src/services/weatherService.js
+++ frontend/src/services/weatherService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://skypulse-weather-roh1t3hfd-sanarasheed11222-bytes-projects.vercel.app/api' });
+const API = axios.create({ baseURL: 'https://skypulse-weather-app-nine.vercel.app/api' });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
